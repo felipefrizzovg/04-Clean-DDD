e6f67788caeea7be81a2d082e501b932aab8f32c
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-01-19 15:29:04 -0300

    Caso de uso: Listar comentários da pergunta

--- src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -1,6 +1,5 @@
 import { InMemoryQuestionCommentRepository } from 'test/repositories/in-memory-question-comments-repository'
 import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
-import { makeAnswer } from 'test/factories/make-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 
