525a0bb3c5aa40e945d01703c07050adc45b04e4
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-01-19 15:01:15 -0300

    Caso de uso: Comentar na resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment On Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentário Teste',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comentário Teste',
+    )
+  })
+})
